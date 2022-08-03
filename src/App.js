074424d8932249546816ba87bb49075563580458
Gid3onJura <homebasekorlash@googlemail.com>
2022-08-03 22:21:53 +0200

    theming and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import { TextField } from '@material-ui/core';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f9ac66',
+    },
+    neutral: {
+      main: '#ed6b5b',
+      contrastText: '#fff',
+    },
+  },
+});
+
+const testData = {
+  "eventTitle": "Eldorado",
+  "eventDate": "2022-09-22",
+  "eventCity": "Köln",
+  "barcode": "3fh6kgk3",
+  "firstName": "Tim",
+  "lastName": "Muster"
+};
+
+async function saveTicket() {
+  try {
+    const result = await fetch('http://localhost:3333', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(testData)
+    }).then(res => {
+      return res.json()
+    })
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+function TicketForm() {
+  return (
+    <div className='ticketForm'>
+      <TextField
+        variant='filled'
+        type='text'
+        label='Titel'
+      />
+      <TextField
+        variant='filled'
+        type='date'
+        label='Datum'
+      />
+      <TextField
+        variant='filled'
+        type='text'
+        label='Ort'
+      />
+      <TextField
+        variant='filled'
+        type='text'
+        label='Name'
+      />
+      <TextField
+        variant='filled'
+        type='text'
+        label='Nachname'
+      />
+      <Button
+        startIcon={<SaveIcon />}
+        variant='contained'
+        size='large'
+        onClick={() => saveTicket()}
+      >
+        Ticket
+      </Button>
+    </div>
+  )
+}
+
+function TicketList() {
+  return (
+    <div className='ticketList'>
+
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <div className='ticketManagerContaier'>
+            <TicketForm />
+            <TicketList />
+          </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
